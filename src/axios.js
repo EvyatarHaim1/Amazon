27d86abefe4e9354f3a58ea65e08ee69fa3e13aa
Evyatar Haim <evyataros1@gmail.com>
2020-11-11 01:55:38 +0200

    deploying the app again - the backend and the frontend seperatelly

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const instance = axios.create({
     // The API (cloud function) URL
-    baseURL: `http://localhost:5001/evyatar-1ea31/us-central1/api`
+    baseURL: `https://us-central1-evyatar-1ea31.cloudfunctions.net/api`
+
 });
 
 export default instance;
+
+// local-url for debugging:
+// `http://localhost:5001/evyatar-1ea31/us-central1/api`
